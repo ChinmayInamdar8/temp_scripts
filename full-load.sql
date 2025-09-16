0f68390afe7df344d5b98d3d34c1a24938787608
Chinmay Pramod Inamdar <128704741+ChinmayInamdar8@users.noreply.github.com>
2025-09-16 09:17:18 +0530

    Add files via upload

--- full-load.sql
+++ full-load.sql
@@ -1,74 +1,73 @@
 load data local
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Terminology/sct2_Concept_Full_INT_20250701.txt' 
+
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Terminology/sct2_Concept_Full_IN1000189_20250620.txt ' 
 	into table concept_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local  
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Terminology/sct2_Description_Full-en_INT_20250701.txt'
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Terminology/sct2_Description_Full_IN1000189_20250620.txt'
 	into table description_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Terminology/sct2_TextDefinition_Full-en_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Terminology/sct2_TextDefinition_Full-en_IN1000189_20250620.txt' 
 	into table textdefinition_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-  
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Terminology/sct2_Relationship_Full_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Terminology/sct2_Relationship_Full_IN1000189_20250620.txt' 
 	into table relationship_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
- 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Terminology/sct2_StatedRelationship_Full_INT_20250701.txt'
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Terminology/sct2_StatedRelationship_Full_IN1000189_20250620.txt'
 	into table stated_relationship_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Language/der2_cRefset_LanguageFull-en_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Refset/Language/der2_cRefset_LanguageFull_IN1000189_20250620.txt' 
 	into table langrefset_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local  
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Content/der2_cRefset_AssociationFull_INT_20250701.txt'
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Refset/Content/der2_cRefset_AssociationFull_IN1000189_20250620.txt'
 	into table associationrefset_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Content/der2_cRefset_AttributeValueFull_INT_20250701.txt'
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-2/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Full/Refset/Content/der2_cRefset_AttributeValueFull_IN1000189_20250620.txt'
 	into table attributevaluerefset_f
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Map/der2_sRefset_SimpleMapFull_INT_20250701.txt' 
-	into table simplemaprefset_f
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Map/der2_sRefset_SimpleMapFull_INT_20250701.txt' 
+-- 	into table simplemaprefset_f
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
 
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Contentder2_Refset_SimpleFull_INT_20250701.txt' 
-	into table simplerefset_f
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Content/der2_Refset_SimpleFull_INT_20250701.txt' 
+-- 	into table simplerefset_f
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
 
 -- load data local 
 -- 	infile 'RF2Release/Full/Refset/Map/der2_iissscRefset_ComplexMapFull_INT_20180131.txt' 
@@ -77,9 +76,9 @@ load data local
 -- 	lines terminated by '\r\n' 
 -- 	ignore 1 lines;
 
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Map/der2_iisssccRefset_ExtendedMapFull_INT_20250701.txt ' 
-	into table extendedmaprefset_f
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Full/Refset/Map/der2_iisssccRefset_ExtendedMapFull_INT_20250701.txt ' 
+-- 	into table extendedmaprefset_f
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
--- snapshot-load.sql
+++ snapshot-load.sql
@@ -1,54 +1,54 @@
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Terminology/sct2_Concept_Snapshot_INT_20250701.txt' 
+load data local
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Terminology/sct2_Concept_Snapshot_IN1000189_20250620T120000Z.txt' 
 	into table concept_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local   
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Terminology/sct2_Description_Snapshot-en_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Terminology/sct2_Description_Snapshot-en_IN1000189_20250620T120000Z.txt' 
 	into table description_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Terminology/sct2_TextDefinition_Snapshot-en_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Terminology/sct2_TextDefinition_Snapshot-en_IN1000189_20250620T120000Z.txt' 
 	into table textdefinition_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Terminology/sct2_Relationship_Snapshot_INT_20250701.txt'
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Terminology/sct2_Relationship_Snapshot_IN1000189_20250620T120000Z.txt'
 	into table relationship_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Terminology/sct2_StatedRelationship_Snapshot_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Terminology/sct2_StatedRelationship_Snapshot_IN1000189_20250620T120000Z.txt' 
 	into table stated_relationship_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot-en_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot-en_IN1000189_20250620T120000Z.txt' 
 	into table langrefset_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local  
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Content/der2_cRefset_AssociationSnapshot_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Refset/Content/der2_cRefset_AssociationSnapshot_IN1000189_20250620T120000Z.txt' 
 	into table associationrefset_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
 	ignore 1 lines;
 
 load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Content/der2_cRefset_AttributeValueSnapshot_INT_20250701.txt' 
+	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/india_extension/part-1/SnomedCT_IndiaDrugExtensionRF2_PRODUCTION_IN1000189_20250620T120000Z/Snapshot/Refset/Content/der2_cRefset_AttributeValueSnapshot_IN1000189_20250620T120000Z.txt' 
 	into table attributevaluerefset_s
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
@@ -57,19 +57,19 @@ load data local
 
 
 
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Map/der2_sRefset_SimpleMapSnapshot_INT_20250701.txt' 
-	into table simplemaprefset_s
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Map/der2_sRefset_SimpleMapSnapshot_INT_20250701.txt' 
+-- 	into table simplemaprefset_s
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
 
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Content/der2_Refset_SimpleSnapshot_INT_20250701.txt' 
-	into table simplerefset_s
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Content/der2_Refset_SimpleSnapshot_INT_20250701.txt' 
+-- 	into table simplerefset_s
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
 
 -- load data local 
 -- 	infile 'RF2Release/Snapshot/Refset/Map/der2_iissscRefset_ComplexMapSnapshot_INT_20180131.txt' 
@@ -78,9 +78,9 @@ load data local
 -- 	lines terminated by '\r\n' 
 -- 	ignore 1 lines;
 	
-load data local 
-	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Map/der2_iisssccRefset_ExtendedMapSnapshot_INT_20250701.txt' 
-	into table extendedmaprefset_s
-	columns terminated by '\t' 
-	lines terminated by '\r\n' 
-	ignore 1 lines;
+-- load data local 
+-- 	infile '/home/opc/project/myclinc-healthnexaa/Product\ Document/snomedct_International_1/SnomedCT_InternationalRF2_PRODUCTION_20250701T120000Z/Snapshot/Refset/Map/der2_iisssccRefset_ExtendedMapSnapshot_INT_20250701.txt' 
+-- 	into table extendedmaprefset_s
+-- 	columns terminated by '\t' 
+-- 	lines terminated by '\r\n' 
+-- 	ignore 1 lines;
